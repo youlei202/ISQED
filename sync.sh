505703a0afb21762830117bd55822ad458a6cf05
youlei202 <lei.you@pm.me>
2025-12-11 16:23:26 +0100

    sync updated

--- sync.sh
+++ sync.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-scp -r "leiyo@login1.hpc.dtu.dk:/zhome/b2/8/197929/GitHub/ISQED-Prototype/results/tables/*" "./results/tables/"
+
+# 1. 下载 tables
+scp -r leiyo@login1.hpc.dtu.dk:/zhome/b2/8/197929/GitHub/ISQED-Prototype/results/tables/* ./results/tables/
+
+# 2. 下载 artifacts
 scp -r leiyo@login1.hpc.dtu.dk:/zhome/b2/8/197929/GitHub/ISQED-Prototype/results/artifacts/* ./results/artifacts/
